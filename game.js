df6c4bdece445911c5c6e91ba91b40b1ace4c3ac
Prakhar Verma <pv594@nyu.edu>
2014-10-29 17:46:32 -0400

    Scale on phone test 2

--- game.js
+++ game.js
@@ -4,35 +4,41 @@ angular.module('myApp',['ngDraggable'])
   .controller('Ctrl', function (
       $window, $scope, $log, $timeout,
       gameService, gameLogic) {
-    
-    
+
+
     //Globals to detect 2 clicks then make move
     var pawnPosition = {row:'',col:''};
     var pawnDelta = {row:'',col:''};
     var lastSelected = {row:'', col:''};
     var movCtr = 2;
     var moveType = 2;
-      
+
     var sound = new Audio("audio/move.wav");
     sound.load();
-    
+
     function sendComputerMove() {
       gameService.makeMove(
           gameLogic.createComputerMove($scope.jsonState,$scope.turnIndex));
     }
-    
+
     $scope.onDropComplete = function (data, event, rowData, colData) {
         $log.info("onDropComplete happened!", arguments);
         $scope.notifications = "Dropped piece " + data + " in " + rowData + "x" + colData;
         $scope.cellClicked(rowData, colData);
       };
-    
-   	$scope.isPawn = function(row,col,pawn){
+
+    $scope.onDrag = function (data, event, rowData, colData) {
+        $log.info("drag happened!", arguments);
+        $scope.notifications = "Dragged " + data + " in " + rowData + "x" + colData;
+        $scope.cellClicked(rowData, colData);
+      };
+
+     $scope.isPawn = function(row,col,pawn){
     	if($scope.board[row][col]===pawn){
     		return true;
     	}
     }
-    
+
     $scope.isWTurn = function(){
     	if($scope.turnIndex===0){
     	return true;
@@ -41,7 +47,7 @@ angular.module('myApp',['ngDraggable'])
     	return false;
     	}
     }
-    
+
     $scope.isSelected = function(row,col){
     	if(row===lastSelected.row && col===lastSelected.col){
 //     		console.log('Found true');
@@ -52,7 +58,7 @@ angular.module('myApp',['ngDraggable'])
     		return false;
     	}
     }
-    
+
     $scope.isBTurn = function(){
     	if($scope.turnIndex===1){
     	return true;
@@ -61,7 +67,7 @@ angular.module('myApp',['ngDraggable'])
     	return false;
     	}
     }
-     
+
     function updateUI(params) {
       $scope.jsonState = angular.toJson(params.stateAfterMove, true);
       $scope.board = params.stateAfterMove.board;
@@ -72,41 +78,41 @@ angular.module('myApp',['ngDraggable'])
       {
       	sound.play();
       }
-      
+
       $scope.isYourTurn = params.turnIndexAfterMove >= 0 && // game is ongoing
         params.yourPlayerIndex === params.turnIndexAfterMove; // it's my turn
       $scope.turnIndex = params.turnIndexAfterMove;
-     
+
      // Is it the computer's turn?
       if ($scope.isYourTurn
           && params.playersInfo[params.yourPlayerIndex].playerId === '') {
         // Wait 500 milliseconds until animation ends.
         $timeout(sendComputerMove, 500);
       }
-    } 
-      
+    }
+
     //initialise the game using this function call to updateUI
     updateUI({stateAfterMove: {}, turnIndexAfterMove: 0, yourPlayerIndex: -2});
-    
-    
+
+
     $scope.cellClicked = function (row, col) {
       $log.info(["Clicked on cell:", row, col]);
       if (!$scope.isYourTurn) {
         return;
       }
-      
+
       if ((($scope.board[row][col]==='A' && $scope.turnIndex===0) ||
       	  	($scope.board[row][col]==='B' && $scope.turnIndex===1)) &&
-      	  	(movCtr===2)){	
+      	  	(movCtr===2)){
       	pawnPosition.row = row;
       	pawnPosition.col = col;
       	movCtr-=1;
       }
-      
-      else if ($scope.board[row][col]==='' && pawnPosition.row !== '' ){	
+
+      else if ($scope.board[row][col]==='' && pawnPosition.row !== '' ){
       	pawnDelta.row = row;
       	pawnDelta.col = col;
-      	
+
       	if(gameLogic.horizontalMoveCheck(pawnPosition,pawnDelta,$scope.board)||
           	gameLogic.verticalMoveCheck(pawnPosition,pawnDelta,$scope.board)  ||
           	gameLogic.diagonalMoveCheck(pawnPosition,pawnDelta,$scope.board)){
@@ -115,11 +121,11 @@ angular.module('myApp',['ngDraggable'])
         else
         	{pawnDelta.row = '';pawnDelta.col = '';}
       }
-      
-      
+
+
       if(movCtr===0)
       {
-		try 
+		try
        	{
 			var move = gameLogic.createMove(pawnPosition, pawnDelta, $scope.turnIndex,
 			$scope.jsonState);
@@ -133,19 +139,19 @@ angular.module('myApp',['ngDraggable'])
         	{pawnPosition = {row:'',col:''};
         	movCtr=2
         	}
-        	pawnDelta = {row:'',col:''};	
-        	gameService.makeMove(move);			
-		} 
-      	catch (e) 
+        	pawnDelta = {row:'',col:''};
+        	gameService.makeMove(move);
+		}
+      	catch (e)
       	{
         	$log.info(["False move", row, col]);
         	return;
       	}
       }
     };
-    
-//     scaleBodyService.scaleBody({width: 152, height: 152});   
-    
+
+//     scaleBodyService.scaleBody({width: 152, height: 152});
+
     gameService.setGame({
       gameDeveloperEmail: "prakhar05@gmail.com",
       minNumberOfPlayers: 2,
