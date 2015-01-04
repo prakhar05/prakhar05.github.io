56a1a0082faf98bc53553df64fea5d59321f1f43
Prakhar Verma <pv594@nyu.edu>
2015-01-04 13:13:23 +0530

    Customising ng-draggable for my game

--- game.js
+++ game.js
@@ -42,11 +42,9 @@ angular.module('myApp',['ngDraggable'])
       if($scope.board[row][col]==='A' ||
         $scope.board[row][col]==='B' ||
         $scope.board[row][col]==='X'){
-        $log.info("is pawn", $scope.board[row][col]);
         return false;
       }
       else
-        $log.info("is not pawn", $scope.board[row][col]);
         return true;
     }
 
@@ -98,7 +96,7 @@ angular.module('myApp',['ngDraggable'])
       if ($scope.isYourTurn
           && params.playersInfo[params.yourPlayerIndex].playerId === '') {
         // Wait 500 milliseconds until animation ends.
-        $timeout(sendComputerMove, 500);
+        $timeout(sendComputerMove, 1000);
       }
     }
 
